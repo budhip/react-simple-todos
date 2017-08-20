ab3757273bf7fd67fa18941f7f36f741b3bd6c3b
budhip <ryo.bunraku@yahoo.com>
2017-08-20 11:52:04 +0700

    create todo component

---
+++ client/src/TodoComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+// import './App.css';
+
+class TodoComponent extends Component {
+  render() {
+    return (
+      <h1>Hello</h1>
+    );
+  }
+}
+
+export default TodoComponent;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import TodoComponent from './TodoComponent';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TodoComponent />, document.getElementById('root'));
 registerServiceWorker();
