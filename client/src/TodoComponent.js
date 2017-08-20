be7c43ec420026257003c6149c30ff65c04d194c
budhip <ryo.bunraku@yahoo.com>
2017-08-20 12:38:35 +0700

    TodoComponent render

--- client/src/TodoComponent.js
+++ client/src/TodoComponent.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 // import './App.css';
 
 class TodoComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: ['wash up!', 'eat some cheese!', 'take a nap!', 'buy flowers!']
+    }
+  }
+
   render() {
+    var todos = this.state.todos;
+    todos = todos.map(function(item, index){
+      return (
+        <li>{item}</li>
+      );
+    });
+
     return (
-      <h1>Hello</h1>
+      <div id="todo-list">
+        <p>The busiest people have the most leisure...</p>
+        <ul>{todos}</ul>
+      </div>
     );
   }
 }
